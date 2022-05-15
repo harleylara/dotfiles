0c1af5cedc085e8d6d9d0ca634e7555c98fa59f0
Harley Lara <harleynelsonlaraalonso1998@gmail.com>
2022-05-15 18:50:59 +0200

    bash setup script

---
+++ bash/setup.bash
@@ -0,0 +1,4 @@
+# echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi" >> ~/.bashrc
+# echo "if [ -f ~/.bash_functions ]; then . ~/.bash_functions; fi" >> ~/.bashrc
+
+echo "neofetch" >> ~/.bashrc
