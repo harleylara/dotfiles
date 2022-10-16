f4081961b7ddf8b839aacb0b768da1571d586125
harleylara <harley.lara@outlook.com>
2022-10-16 15:41:17 +0200

    harpoon keymaps

--- nvim/.config/nvim/lua/harleylara/keymaps.lua
+++ nvim/.config/nvim/lua/harleylara/keymaps.lua
@@ -42,6 +42,15 @@ keymap.set('n', '<leader>fs', ':Telescope file_browser<CR>')
 
 -- Neogit keymaps
 keymap.set('n', '<leader>g', ':Neogit<CR>')
+
+-- Harpoon keymaps
+keymap.set('n', '<leader>ha', ':lua require("harpoon.mark").add_file()<CR>')
+keymap.set('n', '<leader>hl', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
+keymap.set('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<CR>')
+keymap.set('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<CR>')
+keymap.set('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<CR>')
+keymap.set('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<CR>')
+
 -- new tab
 -- keymap.set('n', 'te', ':tabedit')
 
