7b2cfa2ed8f74a70a94a15540b03de5bd10f5dba
harleylara <harley.lara@outlook.com>
2023-01-12 22:18:43 +0100

    more harpons xD

--- nvim/.config/nvim/lua/harleylara/keymaps.lua
+++ nvim/.config/nvim/lua/harleylara/keymaps.lua
@@ -57,6 +57,8 @@ keymap.set('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<CR>')
 keymap.set('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<CR>')
 keymap.set('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<CR>')
 keymap.set('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<CR>')
+keymap.set('n', '<leader>5', ':lua require("harpoon.ui").nav_file(5)<CR>')
+keymap.set('n', '<leader>6', ':lua require("harpoon.ui").nav_file(6)<CR>')
 
 -- new tab
 -- keymap.set('n', 'te', ':tabedit')
