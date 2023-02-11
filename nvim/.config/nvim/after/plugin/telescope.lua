8820371065aeba0e7c7c4e46f886fb769892451b
harleylara <harley.lara@outlook.com>
2023-02-11 19:53:34 +0100

    no more node_modules in telescope yeah

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ if (not status) then return end
 
 telescope.setup {
     -- all mapping on lua/harleylara/keymaps.lua
+    defaults = { file_ignore_patterns = { "node_modules" }}
 }
 
 require("telescope").load_extension "file_browser"
