bc3a53288f34188d11ef43cdfedefc5efd2ad609
harleylara <harley.lara@outlook.com>
2023-04-14 14:06:07 -0700

    idontknow i dont remember using this before

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -6,5 +6,3 @@ telescope.setup {
     -- all mapping on lua/harleylara/keymaps.lua
     defaults = { file_ignore_patterns = { "node_modules" }}
 }
-
-require("telescope").load_extension "file_browser"
