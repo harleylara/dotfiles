3547b006c676d415dd9c3100cd23abb52192a542
harleylara <harley.lara@outlook.com>
2023-07-18 19:34:01 +0200

    fix on_attach

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -16,7 +16,7 @@ return {
 
         -- Use an on_attach function to only map the following keys
         -- after the language server attaches to the current buffer
-        on_attach = function(client, bufnr)
+        local on_attach = function(client, bufnr)
             -- Enable completion triggered by <c-x><c-o>
             vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -25,8 +25,8 @@ return {
             local bufopts = { noremap=true, silent=true, buffer=bufnr }
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-            -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+            vim.keymap.net('n', 'gi', vim.lsp.buf.implementation, bufopts)
             -- vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
             -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
             -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
