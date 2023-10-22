41278d17548c024756ea0e468f7a5d39cc98ce2e
harleylara <harley.lara@outlook.com>
2023-10-22 13:48:15 +0200

    setup autopair

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,19 @@
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    dependencies = {
-        'windwp/nvim-ts-autotag',
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        dependencies = {
+            'windwp/nvim-ts-autotag',
+        },
+        opts = {
+            disable_filetype = { "TelescopePrompt" , "vim" },
+        }
     },
-    opts = {
-        disable_filetype = { "TelescopePrompt" , "vim" },
+    {
+        "windwp/nvim-ts-autotag",
+        -- ft = { "typescriptreact", "tsx" }, -- the plugin will load depend on file type (check lazy.nvim docs)
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
     }
 }
