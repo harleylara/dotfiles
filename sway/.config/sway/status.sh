7e8c2ab3d5c1c7beb8957d4d7900deb4a663b83c
harleylara <harley.lara@outlook.com>
2024-01-28 13:10:05 +0100

    add status information to swaybar

---
+++ sway/.config/sway/status.sh
@@ -0,0 +1,28 @@
+date_formatted=$(date "+%a %F %H:%M")
+
+battery_status=$(cat "/sys/class/power_supply/BAT0/status")
+battery_capacity=$(cat "/sys/class/power_supply/BAT0/capacity")
+
+# Check if battery is discharging
+if [ $battery_status = "Discharging" ]; then
+    battery_info="Batt: $battery_capacity%"
+else
+    battery_info="$battery_status $battery_capacity%"
+fi
+
+current_ssid=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2)
+
+# Check if the SSID is available
+if [ -n "$current_ssid" ]; then
+    network_info="Network: $current_ssid"
+else
+    network_info="Not connected"
+fi
+
+audio_volume=$(amixer -M get Master |\
+    awk '/Mono.+/ {print $6=="[off]" ?\
+    $4" 🔇": \
+    $4" 🔉"}' |\
+    tr -d [])
+
+echo "$audio_volume | $network_info | $battery_info | $date_formatted"
