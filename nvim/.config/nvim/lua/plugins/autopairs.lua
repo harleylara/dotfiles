743604d055956b93f0402e81adb298ce8e9c1d31
harleylara <harley.lara@outlook.com>
2024-03-03 19:57:18 +0100

    change to opts

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -11,9 +11,6 @@ return {
     },
     {
         "windwp/nvim-ts-autotag",
-        -- ft = { "typescriptreact", "tsx" }, -- the plugin will load depend on file type (check lazy.nvim docs)
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end,
+        opts = {}
     }
 }
