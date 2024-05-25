aa350947ee9c1a185008fc22c924dbc227a79655
harleylara <harley.lara@outlook.com>
2024-05-25 18:37:14 +0200

    fix harpoon keymaps

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -15,10 +15,10 @@ return {
         vim.keymap.set('n', '<leader>ha', harpoon_mark.add_file)
         vim.keymap.set('n', '<leader>hl', harpoon_ui.toggle_quick_menu)
         vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 1))
-        vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 2))
-        vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 3))
-        vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 4))
-        vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 5))
-        vim.keymap.set('n', '<leader>1', wrap(harpoon_ui.nav_file, 6))
+        vim.keymap.set('n', '<leader>2', wrap(harpoon_ui.nav_file, 2))
+        vim.keymap.set('n', '<leader>3', wrap(harpoon_ui.nav_file, 3))
+        vim.keymap.set('n', '<leader>4', wrap(harpoon_ui.nav_file, 4))
+        vim.keymap.set('n', '<leader>5', wrap(harpoon_ui.nav_file, 5))
+        vim.keymap.set('n', '<leader>6', wrap(harpoon_ui.nav_file, 6))
     end
 }
