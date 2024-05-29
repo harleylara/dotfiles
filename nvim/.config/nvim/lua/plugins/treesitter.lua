8cde04081278d9a7780907e8ac96f01ce6dde35d
harleylara <harley.lara@outlook.com>
2024-05-29 13:40:40 +0200

    disable highlight for markdown with treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,10 @@ return {
                 -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
                 -- the name of the parser)
                 -- list of language that will be disabled
-                -- disable = {"latex"},
+
+                -- Harley's note: disabled markdown to use the default
+                -- highlighting wich is way better then TS mardown
+                disable = {"markdown"},
                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                 -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -40,9 +43,13 @@ return {
                 'markdown_inline',
                 'cmake',
                 'xml',
+                'vim', -- to remove the default parser and fix errors, https://www.reddit.com/r/neovim/comments/zu9fdc/comment/j1jfzz6/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
             },
             auto_install = true
         }
+
+
+
         require('nvim-treesitter.configs').setup(opts)
     end
 }
