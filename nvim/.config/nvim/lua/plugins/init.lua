62b0dfd52b30fece6c6d50932cfb36e0ecefb10d
harleylara <harley.lara@outlook.com>
2024-06-06 22:54:52 +0200

    add markdown preview

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -15,4 +15,12 @@ return {
     },
 
     'lervag/vimtex',
+
+    -- install without yarn or npm
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    }
 }
