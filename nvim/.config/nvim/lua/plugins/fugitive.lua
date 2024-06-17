8ffef7d730b4dffc53fcc1ae4eda250c6d3bf3f6
harleylara <harley.lara@outlook.com>
2024-06-17 12:01:43 +0200

    another try to neogit

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,23 @@
+-- return {
+--     'tpope/vim-fugitive',
+--     config = function()
+--         vim.keymap.set('n', '<leader>g', ':Git<CR>')
+--     end
+-- }
 return {
-    'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set('n', '<leader>g', ':Git<CR>')
+    "NeogitOrg/neogit",
+    dependencies = {
+
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+    },
+    config = function ()
+        local neogit = require('neogit')
+        neogit.setup {}
+        vim.keymap.set('n', '<leader>g', neogit.open)
     end
 }
