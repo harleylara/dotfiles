7ed48d041cc1f2306972a0c083843cd370a4b239
harleylara <harley.lara@outlook.com>
2024-07-26 14:23:38 +0200

    add better latex integration

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,9 +2,25 @@ return {
     'nvim-telescope/telescope.nvim',
     event = "VimEnter",
     version= '0.1.x',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'jmbuhr/telescope-zotero.nvim',
+            dependencies = {
+                { 'kkharji/sqlite.lua' },
+            },
+            config = function()
+                require("zotero").setup({
+                    zotero_db_path = '/mnt/c/Users/Harley Lara/Zotero/zotero.sqlite',
+                    better_bibtex_db_path = '/mnt/c/Users/Harley Lara/Zotero/better-bibtex.sqlite',
+                })
+            end
+        }
+    },
     config = function()
-        require('telescope').setup{ 
+        local telescope = require("telescope")
+
+        telescope.setup{ 
             defaults = { 
                 file_ignore_patterns = { 
                     "node_modules",
@@ -12,12 +28,17 @@ return {
                 }
             }
         }
+
+        telescope.load_extension("zotero")
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[f]ind [f]ile in the current directory and children'})
 
         vim.keymap.set('n', '<C-f>', builtin.current_buffer_fuzzy_find, { desc = 'Search into the current buffer'})
 
         vim.keymap.set('n', '<leader>hh', builtin.help_tags, { desc = '[h]elp [h]elp'})
+
+        vim.keymap.set('n', '<leader>fz', telescope.extensions.zotero.zotero)
 
         local find_dotfiles = function()
             return builtin.find_files({
@@ -28,5 +49,6 @@ return {
         end
 
         vim.keymap.set('n', '<leader>fd', find_dotfiles, { desc = '[f]ind files intro my dotfiles directory'})
+
     end
 }
