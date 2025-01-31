56c7b9b0b545364468c0e2fdcde3901422d7fabb
harleylara <harley.lara@outlook.com>
2025-01-31 01:21:43 +0100

    add luasnippet

--- nvim/.config/nvim/lua/harleylara/luasnippets/all.lua
+++ nvim/.config/nvim/lua/harleylara/luasnippets/all.lua
@@ -4,5 +4,13 @@ return {
     ls.snippet(
         { trig = "hi" },
         { ls.text_node("Hello world") }
+    ),
+    ls.snippet(
+        { trig = "open" },
+        { ls.text_node("$\\textrm{OPEN}$") }
+    ),
+    ls.snippet(
+        { trig = "closed" },
+        { ls.text_node("$\\textrm{CLOSED}$") }
     )
 }
