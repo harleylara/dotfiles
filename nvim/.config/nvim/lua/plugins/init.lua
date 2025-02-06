97407bb5163c346c770547e797162ef8968ac7c0
harleylara <harley.lara@outlook.com>
2025-02-06 01:21:46 +0100

    add defaults explicitly

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -60,6 +60,12 @@ return {
             require('markdown-table-mode').setup({
                 filetype = {
                     '*.md', '*.mdx'
+                },
+                options = {
+                    insert = true, -- when typing "|"
+                    insert_leave = true, -- when leaving insert
+                    pad_separator_line = false, -- add space in separator line
+                    alig_style = 'default', -- default, left, center, right
                 }}
             )
         end
